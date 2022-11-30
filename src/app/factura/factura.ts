8cebcb7ef82dc3d300c0c1b7a7f0a1aaa599f21d
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 20:12:45 -0500

    Agregar atributos y constructor a factura class

--- src/app/factura/factura.ts
+++ src/app/factura/factura.ts
@@ -1,2 +1,19 @@
+import { Cliente } from '../cliente/cliente';
+import { Asesor } from '../asesor/asesor';
+
+
 export class Factura {
+  id: number;
+  fecha: any;
+  total: number;
+  cliente: Cliente;
+  asesor: Asesor;
+
+  constructor(id:number, fecha:any, total:number, cliente:Cliente, asesor:Asesor) {
+    this.id = id;
+    this.fecha = fecha;
+    this.total = total;
+    this.cliente = cliente;
+    this.asesor = asesor;
+  }
 }
