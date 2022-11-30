b977bf3ffc9fee6d99e26caacd700ed0d665d55b
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 20:05:55 -0500

    Agregar atributos y constructor a isla

--- src/app/isla/isla.ts
+++ src/app/isla/isla.ts
@@ -1,2 +1,11 @@
 export class Isla {
+  id: number;
+  centroComercial: string;
+  ciudad: string;
+
+  constructor(id: number, centroComercial: string, ciudad: string) {
+    this.id = id;
+    this.centroComercial = centroComercial;
+    this.ciudad = ciudad;
+  }
 }
