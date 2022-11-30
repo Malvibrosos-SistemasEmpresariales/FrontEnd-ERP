379a65c490ce321c2608ef401a1ba88621411fac
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 20:09:25 -0500

    Agregar atributos y constructor a clase cliente

--- src/app/cliente/cliente.ts
+++ src/app/cliente/cliente.ts
@@ -1,2 +1,15 @@
 export class Cliente {
+  id: number;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  tipoDocumento: string;
+
+  constructor(id:number, nombre:string, apellido:string, correo:string, tipoDocumento:string) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.tipoDocumento = tipoDocumento;
+  }
 }
