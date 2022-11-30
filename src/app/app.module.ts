9a7081da7e5dab7abe9fdf28036cf07c36c042d6
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 19:12:01 -0500

    Add modules to front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FacturaModule } from './factura/factura.module';
+import { InventarioModule } from './inventario/inventario.module';
+import { ProductoModule } from './producto/producto.module';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FacturaModule,
+    InventarioModule,
+    ProductoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
