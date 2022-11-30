4886e5b3831b6e68ff7dafa29b7995059cd11c09
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 20:08:04 -0500

    Crear atributos y constructor de asesor

--- src/app/asesor/asesor.ts
+++ src/app/asesor/asesor.ts
@@ -1,2 +1,19 @@
+import { Isla } from '../isla/isla';
+
 export class Asesor {
+  id: number;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  tipoDocumento: string;
+  isla: Isla;
+
+  constructor(id:number, nombre:string, apellido:string, correo:string, tipoDocumento:string, isla:Isla) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.tipoDocumento = tipoDocumento;
+    this.isla = isla;
+  }
 }
