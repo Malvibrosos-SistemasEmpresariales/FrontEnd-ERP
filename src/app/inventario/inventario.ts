4b299af861b7455aae62242c01ff5ee9183bb823
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 20:14:56 -0500

    Agregar atributos y constructor a clase inventario

--- src/app/inventario/inventario.ts
+++ src/app/inventario/inventario.ts
@@ -1,2 +1,14 @@
+import { Isla } from "../isla/isla";
+import { Producto } from "../producto/producto"
+
 export class Inventario {
+  cantidad: number;
+  isla: Isla;
+  producto: Producto;
+
+  constructor(cantidad:number, isla:Isla, producto:Producto) {
+    this.cantidad = cantidad;
+    this.isla = isla;
+    this.producto = producto;
+  }
 }
