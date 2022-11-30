313769c1104dc60f72760bb5b26279a52b5bec28
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 19:19:44 -0500

    Create constructor and attributes for producto

--- src/app/producto/producto.ts
+++ src/app/producto/producto.ts
@@ -1,2 +1,13 @@
 export class Producto {
+  codigo: string;
+  nombre: string;
+  valor: number;
+  categoria: string;
+
+  constructor(codigo: string, nombre: string, valor: number, categoria: string) {
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.valor = valor;
+    this.categoria = categoria;
+  }
 }
