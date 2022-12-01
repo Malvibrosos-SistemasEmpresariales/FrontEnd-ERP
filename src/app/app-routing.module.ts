1ee06dba42a5c68807eb544c72e973a008da30e2
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-12-01 18:00:45 -0500

    Create factura route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FacturaListComponent } from './factura/factura-list/factura-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'facturas', component: FacturaListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
