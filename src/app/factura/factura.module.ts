c095fe6054f78632e93247d90b05ae0f267957b9
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-30 19:31:49 -0500

    Agregar declaración del detalle

--- src/app/factura/factura.module.ts
+++ src/app/factura/factura.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FacturaComponent } from './factura.component';
 import { FacturaListComponent} from './factura-list/factura-list.component';
+import { FacturaDetailComponent } from './factura-detail/factura-detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FacturaComponent, FacturaListComponent],
-  exports: [FacturaComponent, FacturaListComponent]
+  declarations: [FacturaComponent, FacturaListComponent, FacturaDetailComponent],
+  exports: [FacturaComponent, FacturaListComponent, FacturaDetailComponent]
 })
 export class FacturaModule { }
